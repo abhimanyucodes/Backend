5b77a95e44c0d333cf8b58d67f2d53c9af700cd0
abhimanyucodes <iamabhimanyu.in@gmail.com>
2026-06-13 11:59:16 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@
 // dotenv.config({
 //     path: "./env"
 // });
+import { app } from "./app.js";
 import connectDb from "./db/index.js";
 import dns from "dns";
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 
-connectDb();
+connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is listing at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB Connection Failed!!!",err);
+})
